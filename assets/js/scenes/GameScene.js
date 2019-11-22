bb1e80a047dfac674c3b517d7e2641a810ba9714
Jeremy Guan <jeremyguan1@gmail.com>
2019-11-21 18:17:11 -0800

    game consturcting

---
+++ assets/js/scenes/GameScene.js
@@ -0,0 +1,30 @@
+class GameScene extends Phaser.Scene {
+  constructor() {
+    super("Game");
+  }
+
+  create() {
+    var goldPickupAudio = this.sound.add("goldSound", { loop: false, volume: 0.2 });
+
+    this.add.sprite(300, 100, "button1");
+
+    this.chest = new Chest(this, 300, 300, "items");
+
+    this.wall = this.physics.add.image(500, 100, "button1");
+    this.wall.setImmovable();
+
+    this.player = new Player(this, 32, 32, "characters", 0);
+
+    this.physics.add.collider(this.player, this.wall);
+    this.physics.add.overlap(this.player, this.chest, function(player, chest) {
+      chest.destroy();
+      goldPickupAudio.play();
+    });
+
+    this.cursors = this.input.keyboard.createCursorKeys();
+  }
+
+  update() {
+    this.player.update(this.cursors);
+  }
+}
